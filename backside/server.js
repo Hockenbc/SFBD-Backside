b40260377779615af72df3158dc82d770bbf31be
Hockenbc <skylar9112@gmail.com>
2024-12-11 13:39:59 -0500

    fixing

--- backside/server.js
+++ backside/server.js
@@ -10,7 +10,7 @@ app.use(cors());
 
 const storage = multer.diskStorage({
   destination: (req, file, cb) => {
-    cb(null, ".public/images/");
+    cb(null, "./public/images/");
   },
   filename: (req, file, cb) => {
     cb(null, file.originalname);
